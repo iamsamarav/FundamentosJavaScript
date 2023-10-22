ffb1088af9f9600179956c3130f2b852c932b833
Samara Vitória <devsamaravitoria@gmail.com>
2023-10-22 11:04:40 -0300

    Arrow Function

--- Funções/funcoes.js
+++ Funções/funcoes.js
@@ -34,3 +34,13 @@ console.log(generator.next())
 console.log(generator.next())
 console.log(generator.next())
 
+//Expressão de função 
+
+const colleenHoover = function(tragedia) {return console.log(`No livro É assim que acaba aconteceu ${tragedia}`)}
+colleenHoover("do Ryle ser um babaca")
+
+//Arrow Function
+
+const Atlas = eAssimQueComeca => console.log(`O Atlas é maravilhoso no livro ${eAssimQueComeca}`)
+
+Atlas("É assim que acaba")
